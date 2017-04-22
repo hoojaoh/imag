aa128d0510b342fa5b2da34916085c5107c579fc
Matthias Beyer <mail@beyermatthias.de>
2017-04-22 13:20:28 +0200

    Add test whether constants exist in Imag

--- libimagruby/tests/test_methods_present.rb
+++ libimagruby/tests/test_methods_present.rb
@@ -9,6 +9,19 @@ class BasicImagInterfaceTest < Minitest::Test
     # nothing
   end
 
+  def test_constants_exist
+    [ :Logger
+    , :StoreId
+    , :StoreHandle
+    , :FileLockEntryHandle
+    , :EntryHeader
+    , :EntryContent
+    , :VERSION
+    ].each do |k|
+      assert Imag.constants.include? k
+    end
+  end
+
   def test_logger_methods_exist
     [:init, :trace, :dbg, :debug, :info, :warn, :error].each do |m|
       assert Imag::Logger.methods.include? m
