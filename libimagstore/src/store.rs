74522b5309d4d64676c044b878b6904a3c1071f3
Matthias Beyer <mail@beyermatthias.de>
2017-06-04 19:12:03 +0200

    Add debug output to Store::delete

--- libimagstore/src/store.rs
+++ libimagstore/src/store.rs
@@ -556,6 +556,8 @@ impl Store {
     pub fn delete<S: IntoStoreId>(&self, id: S) -> Result<()> {
         let id = try!(id.into_storeid()).with_base(self.path().clone());
 
+        debug!("Deleting id: '{}'", id);
+
         {
             let mut entries = match self.entries.write() {
                 Err(_) => return Err(SE::new(SEK::LockPoisoned, None))
@@ -582,6 +584,7 @@ impl Store {
             }
         }
 
+        debug!("Deleted");
         Ok(())
     }
 
