0e20b25091a41b0092f127df15cd444fb78853ba
Matthias Beyer <mail@beyermatthias.de>
2019-10-24 17:39:38 +0200

    Add extension traits for handling Result<Option<T>, E> conversion
    This extension traits help transforming Result<Option<T>, E> to
    Result<T, E> or transforming an iterator over the former type to an
    iterator over the latter type.
    
    Should be moved to resiter of course, but we need to implement this
    first.
    
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- lib/core/libimagerror/src/iter.rs
+++ lib/core/libimagerror/src/iter.rs
@@ -128,3 +128,49 @@ impl<I, T> TraceIterator<T> for I where
     I: Iterator<Item = Result<T, Error>>
 {}
 
+
+/// Extension trait for doing
+///
+/// ```ignore
+///     Iterator<Item = Result<Option<T>, E>>  ->  Iterator<Item = Result<T, E>>
+/// ```
+///
+pub trait IterInnerOkOrElse<T, E, F>
+    where T: Sized,
+          E: Sized,
+          Self: Iterator<Item = Result<Option<T>, E>> + Sized,
+          F: Fn() -> E,
+{
+    fn map_inner_ok_or_else(self, f: F) -> IterInnerOkOrElseImpl<Self, T, E, F>;
+}
+
+pub struct IterInnerOkOrElseImpl<I, T, E, F>(I, F)
+    where I: Iterator<Item = Result<Option<T>, E>> + Sized,
+          T: Sized,
+          E: Sized,
+          F: Fn() -> E;
+
+impl<I, T, E, F> IterInnerOkOrElse<T, E, F> for I
+    where I: Iterator<Item = Result<Option<T>, E>> + Sized,
+          T: Sized,
+          E: Sized,
+          F: Fn() -> E,
+{
+    fn map_inner_ok_or_else(self, f: F) -> IterInnerOkOrElseImpl<I, T, E, F> {
+        IterInnerOkOrElseImpl(self, f)
+    }
+}
+
+impl<I, T, E, F> Iterator for IterInnerOkOrElseImpl<I, T, E, F>
+    where I: Iterator<Item = Result<Option<T>, E>> + Sized,
+          T: Sized,
+          E: Sized,
+          F: Fn() -> E,
+{
+    type Item = Result<T, E>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.next().map(|e| e.and_then(|opt| opt.ok_or_else(|| (self.1)())))
+    }
+}
+
