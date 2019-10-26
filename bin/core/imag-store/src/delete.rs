c301b4b0ab1840a4545faf6f3de314296fb85645
Matthias Beyer <mail@beyermatthias.de>
2019-10-26 15:00:37 +0200

    Fix: Pass path to ui builder function when creating mock app
    Signed-off-by: Matthias Beyer <mail@beyermatthias.de>

--- bin/core/imag-store/src/delete.rs
+++ bin/core/imag-store/src/delete.rs
@@ -49,6 +49,7 @@ mod tests {
         modulename mock;
         version env!("CARGO_PKG_VERSION");
         with help "imag-store mocking app";
+        with ui builder function crate::ui::build_ui;
     }
     use self::mock::generate_test_runtime;
     use self::mock::reset_test_runtime;
